b0d891be9cb397662db4106464199158213bdf57
Mia Ikechukwu <87150395+Mia-IMN@users.noreply.github.com>
2024-10-09 17:52:51 +0100

    Fixed issue with missing letter during Decryption

--- ceasar_cypher.py
+++ ceasar_cypher.py
@@ -5,36 +5,37 @@ print(logo)
 
 using = True
 
+def ED_crypt(choice, shift_num, message):
+    shift_num = shift_num
+    message = message
+    which = choice
+    EorD = 0
+
+    if which == "e":
+        print("\nHere's your Encoded result: ", end="")
+        for let in message:
+            shift_num %= 5
+            EorD = ord(let) + shift_num
+            for letter in string.printable:
+                if letter == chr(EorD):
+                    print(letter, end="")
+    elif which == "d":
+        print("\nHere's your Decoded result: ", end="")
+        for let in message:
+            shift_num %= 5
+            EorD = ord(let) - shift_num
+            for letter in string.printable:
+                if letter == chr(EorD):
+                    print(letter, end="")
+    else:
+        print("Error!! \nChoose valid option (E or D)!")
+
+
 while using == True:
 
     user_choice = input("Do you want to Encode or Decode a message (E or D): ").lower()
     message = input("Type in your message: ")
     shift_num = int(input("Input shift number: "))
-
-    def ED_crypt(choice, shift_num, message):
-        shift_num = shift_num
-        message = message
-        which = choice
-        EorD = 0
-
-        if which == "e":
-            print("\nHere's your Encoded result: ", end="")
-            for let in message:
-                EorD = ord(let) + shift_num
-                for letter in string.printable:
-                    if letter == chr(EorD):
-                        print(letter, end="")
-        elif which == "d":
-            print("\nHere's your Decoded result: ", end="")
-            for let in message:
-                EorD = ord(let) - shift_num
-                for letter in string.printable:
-                    if letter == chr(EorD):
-                        print(letter, end="")
-
-        else:
-            print("Error!! \nChoose valid option!")
-    
 
     ED_crypt(user_choice, shift_num, message)
 
